0ed4c0490cc7b298b6c3a7ba54d50f4274953da3
Charles Allen <charles@allen-net.com>
2016-03-06 20:10:27 -0800

    Works!

--- recipes-kernel/lepton/files/lepton.c
+++ recipes-kernel/lepton/files/lepton.c
@@ -12,7 +12,7 @@
 #define CLASS_NAME "lepton"
 
 #define BUF_SIZE 164
-#define BITS_PER_WORD 16
+#define BITS_PER_WORD 8
 #define LEPTON_SPEED_HZ 12500000
 
 static struct spi_device* spi_dev = NULL;
@@ -28,33 +28,34 @@ static int dev_release(struct inode *ignore1, struct file *ignore2) {
     return 0;
 }
 static ssize_t dev_read(struct file *f, char *out, size_t len, loff_t *off) {
-    struct spi_device *s_dev;
+    struct spi_device *s_dev = spi_dev;
     struct spi_transfer xfers[1];
     struct spi_message message;
     int result;
 
     if (len != BUF_SIZE) {
         printk(KERN_INFO "Refusing to read length of %d, only accepting %d\n", (int)len, (int)BUF_SIZE);
-        return -1;
+        return -EINVAL;
     }
 
-    s_dev = spi_dev;
     if (s_dev == NULL) {
         printk(KERN_INFO "Tried to read from a closed device\n");
-        return -1;
+        return -EINVAL;
     }
     memset(&xfers[0], 0, sizeof(struct spi_transfer));
 
     xfers[0].len = BUF_SIZE;
     // TODO: dma_pool instead of full allocation
-    //xfers[0].rx_buf = kmalloc(BUF_SIZE, GFP_DMA | GFP_KERNEL);
-    xfers[0].rx_buf = dma_zalloc_coherent(&s_dev->dev, BUF_SIZE, &(xfers[0].rx_dma), GFP_DMA | GFP_KERNEL);
-    xfers[0].speed_hz = s_dev->max_speed_hz;
-    xfers[0].bits_per_word = s_dev->bits_per_word;
-    if(xfers[0].rx_buf == NULL) {
-        printk(KERN_ALERT "Failed to allocated %d DMA bytes for lepton\n", (int)BUF_SIZE);
-        return -1;
+    xfers[0].rx_buf = kmalloc(BUF_SIZE, GFP_DMA | GFP_KERNEL);
+    if(!xfers[0].rx_buf) {
+        printk(KERN_ALERT "Failed to allocate %d bytes for lepton\n", (int)BUF_SIZE);
+        return -EBUSY;
     }
+    //xfers[0].rx_buf = dma_zalloc_coherent(&s_dev->dev, BUF_SIZE, &(xfers[0].rx_dma), GFP_DMA | GFP_KERNEL);
+    xfers[0].speed_hz = LEPTON_SPEED_HZ;
+    xfers[0].bits_per_word = BITS_PER_WORD;
+    xfers[0].cs_change = 0;
+    //if(xfers[0].rx_buf == NULL || dma_mapping_error(&s_dev->dev, xfers[0].rx_dma)) {
 
     spi_message_init_with_transfers(
             &message,
@@ -62,19 +63,20 @@ static ssize_t dev_read(struct file *f, char *out, size_t len, loff_t *off) {
             1
     );
     message.spi = s_dev;
+    message.is_dma_mapped = 1;
 
     spi_bus_lock(s_dev->master);
     result = spi_sync_locked(s_dev, &message);
     spi_bus_unlock(s_dev->master);
     if (message.status) {
         printk(KERN_ERR "Failed to read from Lepton SPI: %d\n", (int)message.status);
-        result = -1;
+        result = -EAGAIN;
     } else {
         result = message.actual_length - copy_to_user(out, xfers[0].rx_buf, BUF_SIZE);
     }
 
-    dma_free_coherent(&s_dev->dev, BUF_SIZE, xfers[0].rx_buf, xfers[0].rx_dma);
-    //kfree(xfers[0].rx_buf);
+    //dma_free_coherent(&s_dev->dev, BUF_SIZE, xfers[0].rx_buf, xfers[0].rx_dma);
+    kfree(xfers[0].rx_buf);
     return result;
 }
 
@@ -87,83 +89,31 @@ static struct file_operations fops = {
 static int lepton_spi_probe(struct spi_device *spi) {
     printk(KERN_ALERT "Shouldn't probe!\n");
     return -1;
-    /*
-  printk(KERN_INFO "Probing lepton SPI\n");
-  spi_bus_lock(spi->master);
-  if (spi_dev != NULL) {
-    printk(KERN_INFO "Already have an SPI device, failing probe\n");
-    spi_bus_unlock(spi->master);
-    return -1;
-  }
-  spi->max_speed_hz = LEPTON_SPEED_HZ;
-  spi->bits_per_word = BITS_PER_WORD;
-  spi_setup(spi);
-  spi_dev = spi;
-  lepton_module_running = 1;
-  spi_bus_unlock(spi->master);
-  printk(KERN_INFO "Lepton SPI protocol probed.\n");
-  return 0;
-  */
 }
 
 static int lepton_spi_remove(struct spi_device *spi) {
     printk(KERN_ALERT "Shouldn't remove!\n");
     return -1;
-    /*
-  if (spi != spi_dev) {
-    printk(KERN_INFO "Asked to remove an SPI device we aren't using\n");
-    return -1;
-  }
-  spi_bus_lock(spi->master);
-  spi_dev = NULL;
-  spi_bus_unlock(spi->master);
-  return 0;
-  */
 }
 
-// See https://github.com/primiano/edison-kernel/blob/master/arch/x86/platform/intel-mid/board.c
-// and https://github.com/primiano/edison-kernel/blob/master/drivers/spi/spi-dw-pci.c
-// and https://github.com/primiano/edison-kernel/blob/master/drivers/spi/spi-dw.h
-// and https://github.com/primiano/edison-kernel/blob/master/drivers/spi/spi.c#L71
-static struct spi_device_id idtable[] = {
-  // Other devices on edison
-  //{"ads7955", 0},
-  //{"spi_max3111", 0},
-  {"spidev", 0},
-  /* Intel Medfield platform SPI controller 1 */
-  //{ PCI_DEVICE(PCI_VENDOR_ID_INTEL, 0x0800), .driver_data = 0 },
-  //{"dw_spi_pci", 0},
-  {}
-};
-static struct spi_driver lepton_spi_driver = {
-  .driver = {
-    .name = "lepton",
-    /** Auto populated
-    .owner = THIS_MODULE,
-    .bus = &spi_bus_type,
-    .probe = lepton_spi_probe,
-    .remove = lepton_spi_remove,
-    */
-  },
-  .probe = lepton_spi_probe,
-  .remove = lepton_spi_remove,
-  .id_table = idtable,
-};
 
 static int __lepton_driver_list(struct device *dev, void *n)
 {
+    struct list_head *dma_head = NULL;
+    struct dma_pool *pool;
     const struct spi_device *spi = to_spi_device(dev);
     printk(KERN_INFO "Found spi device [%s]\n", spi->modalias);
-    return 0;
+    dma_head = &dev->dma_pools;
+    printk(KERN_INFO "With%s DMA pools. With%s DMA mask\n", list_is_singular(dma_head) ? "out" : "", dev->coherent_dma_mask ? "" : "out");
+    return !strcmp(spi->modalias, "spidev");
 }
 
 static int __init lepton_spi_init(void)
 {
     struct spi_master *master = NULL;
-    int res;
     u16 i;
 
-    for(i = 0; i < 10; ++i) {
+    for(i = 0; i < 10 && !spi_dev; ++i) {
         master = spi_busnum_to_master(i);
         if(master == NULL) {
             continue;
@@ -174,34 +124,21 @@ static int __init lepton_spi_init(void)
         } else {
             printk(KERN_INFO "No bus on %d\n", (int) i);
         }
-        device_find_child(&master->dev, NULL, __lepton_driver_list);
+        if(master->dma_alignment) {
+            printk(KERN_INFO "DMA alignment: %d\n", (int) master->dma_alignment);
+        }
+        spi_dev = device_find_child(&master->dev, NULL, __lepton_driver_list);
     }
 
-    master = spi_busnum_to_master(5);
-    spi_dev = spi_alloc_device(master);
-    spi_dev->max_speed_hz = LEPTON_SPEED_HZ;
-    spi_dev->mode = SPI_MODE_3;
-    spi_dev->bits_per_word = BITS_PER_WORD;
-    spi_dev->chip_select = 3;// we don't use it
-    spi_dev->cs_gpio = -ENOENT;
-    res = spi_add_device(spi_dev);
     printk(KERN_INFO "Added lepton device with%s dma\n", spi_dev->dev.dma_mask ? "" : "out");
-    //res = master->setup(&lepton_spi_driver);
-    //res = spi_register_driver(&lepton_spi_driver);
-    if (res)
-      return res;
     major_number = register_chrdev(0, DEVICE_NAME, &fops);
     if(major_number < 0) {
-        //spi_unregister_driver(&lepton_spi_driver);
-        spi_unregister_device(spi_dev);
         printk(KERN_ALERT "Failed to register lepton major number!\n");
         return major_number;
     }
     leptonClass = class_create(THIS_MODULE, CLASS_NAME);
     if (IS_ERR(leptonClass)) {
         unregister_chrdev(major_number, DEVICE_NAME);
-        //spi_unregister_driver(&lepton_spi_driver);
-        spi_unregister_device(spi_dev);
         printk(KERN_ALERT "Failed to register lepton device class!\n");
         return PTR_ERR(leptonClass);
     }
@@ -210,8 +147,6 @@ static int __init lepton_spi_init(void)
         class_unregister(leptonClass);
         class_destroy(leptonClass);
         unregister_chrdev(major_number, DEVICE_NAME);
-        //spi_unregister_driver(&lepton_spi_driver);
-        spi_unregister_device(spi_dev);
         printk(KERN_ALERT "Failed to create lepton device!\n");
         return PTR_ERR(leptonDevice);
     }
@@ -226,7 +161,6 @@ static void __exit lepton_spi_cleanup(void)
     class_unregister(leptonClass);
     class_destroy(leptonClass);
     unregister_chrdev(major_number, DEVICE_NAME);
-    spi_unregister_device(spi_dev);
     printk(KERN_INFO "Lepton unregistered\n");
 }
 
@@ -239,5 +173,4 @@ MODULE_AUTHOR("Charles Allen");
 MODULE_DESCRIPTION("SPI module for the Lepton");
 MODULE_VERSION("0.1");
 MODULE_ALIAS("spi:lepton");
-MODULE_DEVICE_TABLE(spi, idtable);
 
